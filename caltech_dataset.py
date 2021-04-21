d7c39f6cf022452792f7c8527a590ea033f422a8
Raffaella Ciancio <74151831+raffacnc@users.noreply.github.com>
2021-04-21 10:11:34 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -6,10 +6,6 @@ import os
 import os.path
 import sys
 import pandas as pd
-from skimage import io, transform
-import numpy as np
-import matplotlib.pyplot as plt
-
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -21,15 +17,6 @@ def pil_loader(path):
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
-
-        self.split = split  # This defines the split you are going to use
-        # (split files are called 'train.txt' and 'test.txt')
-
-        self.root = root + "/" + split + ".txt"  # Caltech101/train.txt
-        self.caltech_frame = pd.read_csv(self.root, delimiter='\n', header=None)
-        self.transform = transform
-        self.target_transform = target_transform
-
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -39,6 +26,26 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
+        self.split = split # This defines the split you are going to use
+                           # (split files are called 'train.txt' and 'test.txt')
+
+        images_paths = []
+        labels = []
+
+        self.categories = os.listdir(self.root)
+        self.categories.remove("BACKGROUND_Google")
+
+        for path in open(f"./Caltech101/{self.split}.txt"):
+          path = path.replace("\n", "")
+          category = path.split("/")[0]
+          if(category != "BACKGROUND_Google"):
+            image_path = self.root + "/" + path
+            images_paths.append(image_path)
+            labels.append(self.categories.index(category))
+
+        self.data = pd.DataFrame(zip(images_paths, labels), columns = ["image_path", "label"])
+        
+
     def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
@@ -47,27 +54,19 @@ class Caltech(VisionDataset):
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-        img_name = self.caltech_frame.iloc[index, 0] # accordion/image__00x.jpg
-        label = img_name.split("/")[0] #accordion
-        name = img_name.split("/")[1] #image_00x.jpg
-        image = pil_loader("./Caltech101/101_ObjectCategories/" + label + "/" + name)
-
-        #image = Image.fromarray(image)
-        sample = {'image': image, 'label': label}  # Provide a way to access image and label via index
-        # Image should be a PIL Image
-        # label can be int
+        image_path = self.data["image_path"].iloc[index]
+        image, label = pil_loader(image_path), self.data["label"].iloc[index]
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
 
-        return sample
+        return image, label
 
     def __len__(self):
         '''
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-
-        length = self.caltech_frame.size  # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.data) # Provide a way to get the length (number of elements) of the dataset
         return length
