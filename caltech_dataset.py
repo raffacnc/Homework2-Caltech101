9a543883c562255317b6d6bacf85e4482d656909
Raffaella Ciancio <74151831+raffacnc@users.noreply.github.com>
2021-04-21 09:56:56 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -9,6 +9,8 @@ import pandas as pd
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
@@ -17,49 +19,43 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
-    def init(self, root, split='train', transform=None, target_transform=None):
-        super(Caltech, self).init(root, transform=transform, target_transform=target_transform)
+    def __init__(self, root, split='train', transform=None, target_transform=None):
+        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = split  # This defines the split you are going to use
+        # (split files are called 'train.txt' and 'test.txt')
 
-
-        self.root = root+"/"+split+".txt" #./root/train.txt
-        self.caltech_frame = pd.read_csv(self.root, delimiter = '\n', header=None)
+        self.root = root + "/" + split + ".txt"  # Caltech101/train.txt
+        self.caltech_frame = pd.read_csv(self.root, delimiter='\n', header=None)
         self.transform = transform
         self.target_transform = target_transform
-
-
-
-      
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
         - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the getitem method to access the image-label pair
+        - You should provide a way for the __getitem__ method to access the image-label pair
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
-    def getitem(self, index):
+    def __getitem__(self, index):
         '''
-        getitem should access an element through its index
+        __getitem__ should access an element through its index
         Args:
             index (int): Index
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-        img_name = os.path.join(self.root, self.caltech_frame.iloc[index, 0])
-        label = img_name.split("/")[3]
-        
-        name = img_name.split("/")[4]
+        img_name = self.caltech_frame.iloc[index, 0] # accordion/image__00x.jpg
+        label = img_name.split("/")[0] #accordion
+        name = img_name.split("/")[1] #image_00x.jpg
+        image = pil_loader("./Caltech101/101_ObjectCategories/" + label + "/" + name)
 
-        image = io.imread("./root/101_ObjectCategories/"+"/"+label+"/"+name)
-        image = Image.fromarray(image)
-        sample = {'image' : image, 'label' : label} # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        #image = Image.fromarray(image)
+        sample = {'image': image, 'label': label}  # Provide a way to access image and label via index
+        # Image should be a PIL Image
+        # label can be int
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -67,11 +63,11 @@ class Caltech(VisionDataset):
 
         return sample
 
-    def len(self):
+    def __len__(self):
         '''
-        The len method returns the length of the dataset
+        The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
 
-        length = self.caltech_frame.size # Provide a way to get the length (number of elements) of the dataset
+        length = self.caltech_frame.size  # Provide a way to get the length (number of elements) of the dataset
         return length
